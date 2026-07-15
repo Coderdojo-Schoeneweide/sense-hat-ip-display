61396d71d1d31784e684782189a9495a1effc3bd
Bluemi <bruno.schilling@protonmail.ch>
2026-07-15 15:21:59 +0200

    simple project structure

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 case "$1" in
 	r)
 		shift
-		python3 sense-hat-ip-display/cli/main.py "$@"
+		python3 src/sense-hat-ip-display.py "$@"
 		;;
 	t)
 		shift
--- tests/test_main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from sense-hat-ip-display.cli.main import main
-
-
-def test_main():
-    main()
-
